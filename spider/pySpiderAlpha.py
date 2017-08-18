5a2b50efe1ed35f2155a218563a2c8d17b9557a7
wuqf <wuqingfeng234@163.com>
2017-08-18 16:24:12 +0800

    init

---
+++ spider/pySpiderAlpha.py
@@ -0,0 +1,35 @@
+import re
+import urllib.request
+import urllib
+from collections import deque
+
+queue = deque()
+visited = set()
+
+url = 'http://news.dbanotes.net'
+queue.append(url)
+cnt = 0
+while queue:
+    url = queue.popleft()
+    visited |= {url}
+
+    print('complete: ' + str(cnt) + ' Crawling <--- ' + url)
+    cnt += 1
+
+    try:
+        urlop = urllib.request.urlopen(url)
+    except:
+        continue
+
+    if (('html' not in urlop.getheader('Content-Type')) or (200 != urlop.getcode())):
+        continue
+
+    try:
+        data = urlop.read().decode('utf-8')
+    except:
+        continue
+    linkre = re.compile('href=\"(.+?)\"')
+    for x in linkre.findall(data):
+        if 'http' in x and x not in visited:
+            queue.append(x)
+            print('enqueue --->  ' + x)
